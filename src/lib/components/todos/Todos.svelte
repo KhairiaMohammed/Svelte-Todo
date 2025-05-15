04ccdde598c67fb0466a8227eae59de5de41dd4a
Khairiah Mohammed <kh.alayyafi@gmail.com>
2025-05-15 20:04:47 +0300

    new app

--- src/lib/components/todos/Todos.svelte
+++ src/lib/components/todos/Todos.svelte
@@ -1,26 +1,3 @@
-<!-- svelte-ignore export_let_unused -->
-<!-- <script>
-  import Todo from "./Todo.svelte";
-
-  export let todos = []
-  export let activeEditTodo;
-
-  $: activeTodos = activeEditTodo ? [activeEditTodo] : todos
-
-</script>
-
-<div class="todos-list">
-  {#each activeTodos as todo (todo.id) }
-    
-  <Todo  {todo} {activeEditTodo} on:deleteTodo on:editTodo on:toggleTodo/>
-  {/each}
-
-  {#if todos.length === 0}
-    <h3 class="no-todos"> لا يوجد مهام حالية ... </h3>
-  {/if}
-  
-</div> -->
-
 <script>
   import Todo from "./Todo.svelte";
 
--- src/lib/components/todos/TodosForm.svelte
+++ src/lib/components/todos/TodosForm.svelte
@@ -1,55 +1,3 @@
-<!-- <script>
-  import {CircleIcon } from 'svelte-feather-icons'
-  import { createEventDispatcher} from 'svelte'
-  const dispatch = createEventDispatcher()
-
-  export let activeEditTodo;
-  export let isFilter
-
-  let value = ''
-
-  const handleAddTodo = () => {
-    if(!value.trim() ) {
-      return
-    }
-    dispatch('addTodo' , value.trim())
-    value = ''
-  }
-
-  const editChange = () => {
-    if(activeEditTodo){
-      value = activeEditTodo.title
-    }
-  }
-
-  const handleFilter = () => {
-    dispatch('filter'  )
-  }
-
-  $: activeEditTodo, editChange()
-
-</script>
-
-<div class="todos-form">
-  <div class="todos-form_icon" class:active={isFilter} on:click={handleFilter}>
-    <CircleIcon />
-  </div>
-   
-  <div class="todos-form_form">
-    <input bind:value  type="text" placeholder="اضف مهمة جديدة ..." />
-  </div>
-  <div class="todos-form_submit" on:click={handleAddTodo}>
-    <button class="btn" disabled={!value.trim()}> 
-      {
-        activeEditTodo ? 
-        'تعديل' :
-        'اضافة'
-      }
-      
-        </button>
-  </div>
-</div> -->
-
 <script>
   import {CircleIcon } from 'svelte-feather-icons'
   import { createEventDispatcher} from 'svelte'
@@ -83,6 +31,8 @@
 </script>
 
 <div class="todos-form">
+  <!-- svelte-ignore a11y_click_events_have_key_events -->
+  <!-- svelte-ignore a11y_no_static_element_interactions -->
   <div class="todos-form_icon" class:active={isFilter} on:click={handleFilter}>
     <CircleIcon />
   </div>
@@ -90,6 +40,8 @@
   <div class="todos-form_form">
     <input bind:value  type="text" placeholder="اضف مهمة جديدة ..." />
   </div>
+  <!-- svelte-ignore a11y_click_events_have_key_events -->
+  <!-- svelte-ignore a11y_no_static_element_interactions -->
   <div class="todos-form_submit" on:click={handleAddTodo}>
     <button class="btn" disabled={!value.trim()}> 
       {
